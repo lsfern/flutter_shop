d80b4de9121295a1e4f9e23597f5cfd5e7d84eea
unknown <fighting_android@sina.com>
2019-07-24 17:15:56 +0800

    first commit

---
+++ lib/index_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class IndexPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('百姓生活+'),
+        ),
+        body: Center(
+          child: Text('百姓生活+'),
+        ));
+  }
+}
