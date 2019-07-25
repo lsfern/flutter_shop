e6394b37bd9fe56742c0f36d32ed6bd2c4c7a8f1
unknown <fighting_android@sina.com>
2019-07-25 09:01:25 +0800

    update gitignore

--- lib/index_page.dart
+++ lib/index_page.dart
@@ -1,14 +1,16 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+class IndexPage extends StatefulWidget {
+  @override
+  _IndexPageState createState() => _IndexPageState();
+}
 
-class IndexPage extends StatelessWidget {
+class _IndexPageState extends State<IndexPage> {
+  final List<BottomNavigationBar>
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text('百姓生活+'),
-        ),
-        body: Center(
-          child: Text('百姓生活+'),
-        ));
+    return Container(
+
+    );
   }
 }
