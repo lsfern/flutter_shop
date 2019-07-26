271d6abcd523f225e43c860e0d6e34b56d1664c9
unknown <fighting_android@sina.com>
2019-07-26 17:36:43 +0800

    homepage swiper

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'dart:convert';
+
+import 'package:flutter_swiper/flutter_swiper.dart';
 import '../service/server_method.dart';
 
 class HomePage extends StatefulWidget {
@@ -7,23 +10,64 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  String homePageContent = '暂无数据';
-  @override
-  void initState() {
-    getHomePageContent().then((val) {
-      setState(() {
-        homePageContent = val.toString();
-      });
-    });
-    super.initState();
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text('百姓生活+')),
-        body: SingleChildScrollView(
-          child: Text(homePageContent),
+        appBar: AppBar(
+          title: Text('百姓生活+'),
+          elevation: 0.0,
+          centerTitle: true,
+        ),
+        body: FutureBuilder(
+          future: getHomePageContent(),
+          builder: (context, result) {
+            if (result.hasData) {
+              var data = json.decode(result.data.toString());
+              var _data = data['data'];
+              if (_data != null) {
+                List<Map> swiperList = (_data['slides'] as List).cast();
+                return Column(
+                  children: <Widget>[
+                    SwiperDiy(swiperList: swiperList),
+                  ],
+                );
+              } else {
+                return Center(
+                    child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Text('数据加载失败,请检查网络设置'),
+                    RaisedButton(
+                        child: Text('重新加载'),
+                        onPressed: () => getHomePageContent())
+                  ],
+                ));
+              }
+            } else {
+              return Center(child: Text('加载中...'));
+            }
+          },
         ));
+  }
+}
+
+///首页轮播组件
+class SwiperDiy extends StatelessWidget {
+  final List swiperList;
+  SwiperDiy({Key key, this.swiperList}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 170.0,
+      child: Swiper(
+        itemBuilder: (BuildContext context, int index) {
+          return Image.network("${swiperList[index]['image']}",fit: BoxFit.fill,);
+        },
+        itemCount: swiperList.length,
+        pagination: SwiperPagination(),
+        autoplay: true,
+      ),
+    );
   }
 }
--- lib/service/server_method.dart
+++ lib/service/server_method.dart
@@ -5,13 +5,16 @@ import 'service_url.dart';
 
 ///获取首页数据
 Future getHomePageContent() async {
+  print('开始请求首页数据...');
   try {
     Response response;
     Dio dio = new Dio();
     dio.options.contentType =
         ContentType.parse("application/x-www-form-urlencoded");
-    response = await dio.post(servicePath['homePageContent']);
+    var formData = {'lon': '115.02932', 'lat': '35.76189'};
+    response = await dio.post(servicePath['homePageContent'], data: formData);
     if (response.statusCode == 200) {
+      print(response.data);
       return response.data;
     } else {
       throw Exception('后端接口异常');
